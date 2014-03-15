caa70752c3b191da2f9fa7957a9f5b673d11f004
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-03-15 02:02:08 -0500

    convert converter to converting for hugo

--- export.pl
+++ export.pl
@@ -6,10 +6,7 @@ use warnings;
 use XML::Simple;
 use DateTime::Format::Strptime;
 use HTML::WikiConverter;
-use LWP::UserAgent;
-use Try::Tiny;
-use Digest::MD5 'md5_hex';
-use JSON;
+use DateTime::Format::ISO8601;
 use URI;
 use Syntax::Keyword::Gather;
 use HTML::Entities;
@@ -23,23 +20,17 @@ chomp(my $email = qx(git config --get user.email));
 my ($file, $subdir, $branch) = @ARGV;
 
 my %events;
-# generated with merely:
-# perl -MDBI -MJSON -E'my $dbh = DBI->connect("DBI:mysql:database=wordpressdb;host=mysql.afoolishmanifesto.com", "wordpress", "xxx"); print encode_json($dbh->selectall_hashref("SELECT * FROM wp_comments", "comment_ID"))' > out.json
-my %comment_metadata = %{decode_json(do { local (@ARGV, $/ ) = '/home/frew/out.json'; <> })};
-my %backcompat_urlmap;
-my %seen_tags;
 
-POST:
 for my $x (grep $_->{'wp:status'} eq 'publish', @{XMLin($file)->{channel}{item}}) {
    warn " -- importing post: $x->{title}\n";
    state $date_parser = DateTime::Format::Strptime->new(
       pattern => '%F %T',
-      time_zone => 'UTC',
+      time_zone => 'America/Chicago',
    );
 
    my $stub = $x =~ m<([^/]+)\/$>
       ? $1
-      : lc($x->{title} =~ s/\W/-/gr =~ s/-$//r)
+      : lc($x->{title} =~ s/\W/-/gr =~ s/-+/-/gr =~ s/-$//r =~ s/^-//r)
    ;
 
    my $guid = $x->{guid}{content} || $x->{link};
@@ -61,28 +52,23 @@ for my $x (grep $_->{'wp:status'} eq 'publish', @{XMLin($file)->{channel}{item}}
          @$c
       }
    };
-   my %tags_to_create = map { $_ => 1 } @tags;
-   delete $tags_to_create{$_}
-      for keys %seen_tags;
 
-   $seen_tags{$_} = 1 for keys %tags_to_create;
-
-   my $content =
-      sprintf(qq([[!meta title="%s"]]\n), $x->{title} =~ s/"/\\"/gr) .
-      sprintf(qq([[!meta date="%s"]]\n), $posted_at->ymd('-') . q( ) . $posted_at->hms(':')) .
-      (
-         $x->{guid}{content}
-            ? qq([[!meta guid="$guid"]]\n)
-            : '',
-      ) .
-      convert_content($x->{'content:encoded'}) .
+   my $content = "---\n" .
+      sprintf(qq(aliases: ["%s"]\n), URI->new($x->{link})->path) .
+      sprintf(qq(title: "%s"\n), $x->{title} =~ s/"/\\"/gr) .
+      sprintf(qq(date: "%s"\n), format_tz($posted_at)) .
       (
          @tags
-            ? "\n\n[[!tag " . (join ' ', @tags) . ' ]]'
+            ? 'tags: [' . (join ', ', map qq("$_"), @tags) . "]\n"
             : ''
-      );
-
-   $backcompat_urlmap{URI->new($x->{link})->path} = "/posts/$stub/";
+      ) .
+      (
+         $x->{guid}{content}
+            ? qq(guid: "$guid"\n)
+            : '',
+      ) .
+      "---\n" .
+      convert_content($x->{'content:encoded'});
 
    $events{$timestamp} = join "\n",
       "commit refs/heads/$branch",
@@ -92,114 +78,14 @@ for my $x (grep $_->{'wp:status'} eq 'publish', @{XMLin($file)->{channel}{item}}
       $msg,
       '8675309',
 
-      "M 644 inline $subdir/$stub.mdwn",
+      "M 644 inline $subdir/$stub.md",
       'data <<8675309',
       $content,
       '8675309',
-
-      map {;
-         "M 644 inline tags/$_.mdwn",
-         'data <<8675309',
-         qq([[!meta title="pages tagged $_"]]),
-         '',
-         qq([[!inline pages="tagged($_)" actions="no" archive="yes"),
-         'feedshow=10]]',
-         '8675309',
-      } sort keys %tags_to_create;
    ;
-
-   get_comments($x->{link}, "$subdir/$stub")
-      if $x->{'wp:post_type'} eq 'post'
-}
-
-sub get_comments {
-   my ($url, $dir) = @_;
-
-   state $ua = LWP::UserAgent->new(keep_alive => 1);
-
-   my $content = $ua->get("$url/feed")->decoded_content;
-   my $first;
-   my $bail;
-   my $decoded =
-      try { XMLin($content, ForceArray => ['item']) }
-      catch { $bail = 1 };
-
-   return if $bail;
-
-   COMMENT:
-   for my $x (@{$decoded->{channel}{item}}) {
-      my $date = $x->{pubDate};
-      $date =~ s/^\S+\s//;
-      $date =~ s/\s\S+$//;
-
-      #ghetto
-      $date =~ s/Jan/01/;
-      $date =~ s/Feb/02/;
-      $date =~ s/Mar/03/;
-      $date =~ s/Apr/04/;
-      $date =~ s/May/05/;
-      $date =~ s/Jun/06/;
-      $date =~ s/Jul/07/;
-      $date =~ s/Aug/08/;
-      $date =~ s/Sep/09/;
-      $date =~ s/Oct/10/;
-      $date =~ s/Nov/11/;
-      $date =~ s/Dec/12/;
-
-      my $metadata = $comment_metadata{$x->{link} =~ s/^.*#comment-//r};
-
-      state $date_parser = DateTime::Format::Strptime->new(
-         pattern => '%d %m %Y %T',
-         time_zone => 'UTC',
-      );
-
-      my $datetime = $date_parser
-         ->parse_datetime($date);
-
-      my $timestamp = $datetime->epoch;
-      my $formatted_date = "$timestamp";
-      my $email = $metadata->{comment_author_email}
-         || $x->{'dc:creator'} . '@WP';
-
-      my $content = convert_content($x->{'content:encoded'});
-      my $extra = "User-Agent: $metadata->{comment_agent}\n"
-                . "IP-Address: $metadata->{comment_author_IP}\n";
-      my $msg = "Added a comment\n\n$extra";
-      utf8::encode($x->{'dc:creator'});
-      my $committer = "$x->{'dc:creator'} <$email>";
-
-      warn " -- importing comment from $committer\n";
-
-      my $link = '';
-
-      $link = "\n\n[url]($metadata->{comment_author_url})"
-         if $metadata->{comment_author_url};
-      $events{$timestamp} = join "\n",
-         "commit refs/heads/$branch",
-         # still need to get email address
-         "committer $committer $timestamp +0000",
-         'data <<8675309',
-         $msg,
-         '8675309',
-         "M 644 inline " . unique_comment_location($dir, $content),
-         'data <<8675309',
-
-      <<"COMMENT",
-[[!comment format=mdwn
- username="$x->{'dc:creator'}"
- date="$formatted_date"
- content="""
-$content$link
-"""]]
-COMMENT
-      '8675309'
-      ;
-   }
 }
 
 say $events{$_} for sort keys %events;
-open my $fh, '>', 'map.json';
-print {$fh} encode_json(\%backcompat_urlmap);
 
 sub convert_content {
    my $body = shift;
@@ -274,18 +160,16 @@ sub convert_content {
    }
 }
 
-sub unique_comment_location {
-   my ($dir, $content) = @_;
+sub format_tz {
+   my $dt = shift;
 
-   utf8::encode($content);
-   my $md5 = md5_hex($content);
+   my $hour_offset = int($dt->offset / 60 / 60);
+   if ($hour_offset >= 0) {
+      $hour_offset = sprintf("+%02i", $hour_offset)
+   } else {
+      $hour_offset = sprintf("-%02i", -$hour_offset)
+   }
+   my $minute_offset = abs($dt->offset / 60 - int($dt->offset / 60 / 60) * 60);
 
-   my $location;
-   my $i = 0;
-   do {
-      $i++;
-      $location = "$dir/comment_${i}_$md5._comment";
-   } while -e $location;
-
-   return $location
+   sprintf '%sT%s%s:%02i', $dt->ymd('-'), $dt->hms(':'), $hour_offset, $minute_offset;
 }
